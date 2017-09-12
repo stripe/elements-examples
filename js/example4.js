c4ef34e9632f974c95b8432bb023294c2363aa00
Michelle Bu <michelle@michellebu.com>
2017-09-11 20:01:07 -0700

    Add PaymentRequest examples

--- js/example4.js
+++ js/example4.js
@@ -5,6 +5,9 @@
     cssSrc: 'https://rsms.me/interface/interface.css',
   });
 
+  /**
+   * Card Element
+   */
   var card = elements.create('card', {
     style: {
       base: {
@@ -23,7 +26,50 @@
       },
     },
   });
+
   card.mount('#example4-card');
 
-  registerElements([card], 'example4');
+  /**
+   * Payment Request Element
+   */
+  var paymentRequest = stripe._paymentRequestBeta({
+    country: 'US',
+    currency: 'USD',
+    total: {
+      amount:
+        document.querySelector('.example4 input[name="amount"]:checked').value *
+        100,
+      label: 'Total',
+    },
+  });
+  var paymentRequestElement = elements.create('_paymentRequestBeta', {
+    paymentRequest: paymentRequest,
+    style: {
+      _paymentRequestBeta: {
+        type: 'donate',
+      },
+    },
+  });
+
+  document
+    .querySelector('.example4 input[name="amount"]')
+    .addEventListener('change', function(ev) {
+      if (ev.target.checked) {
+        // TODO: This API is not yet implemented.
+        // paymentRequest.update({total: ev.target.value});
+      }
+    });
+
+  paymentRequest.canMakePayment().then(function(result) {
+    if (result.available) {
+      document.querySelector('.example4 .card-only').style.display = 'none';
+      document.querySelector(
+        '.example4 .payment-request-available'
+      ).style.display =
+        'block';
+      paymentRequestElement.mount('#example4-paymentRequest');
+    }
+  });
+
+  registerElements([card, paymentRequestElement], 'example4');
 })();
--- js/example5.js
+++ js/example5.js
@@ -3,6 +3,9 @@
 
   var elements = stripe.elements();
 
+  /**
+   * Card Element
+   */
   var card = elements.create('card', {
     style: {
       iconStyle: 'solid',
@@ -28,6 +31,37 @@
     },
   });
   card.mount('#example5-card');
+
+  /**
+   * Payment Request Element
+   */
+  var paymentRequest = stripe._paymentRequestBeta({
+    country: 'US',
+    currency: 'USD',
+    total: {
+      amount: 2500,
+      label: 'Total',
+    },
+  });
+  var paymentRequestElement = elements.create('_paymentRequestBeta', {
+    paymentRequest: paymentRequest,
+    style: {
+      _paymentRequestBeta: {
+        theme: 'light',
+      },
+    },
+  });
+
+  paymentRequest.canMakePayment().then(function(result) {
+    if (result.available) {
+      document.querySelector('.example5 .card-only').style.display = 'none';
+      document.querySelector(
+        '.example5 .payment-request-available'
+      ).style.display =
+        'block';
+      paymentRequestElement.mount('#example5-paymentRequest');
+    }
+  });
 
   registerElements([card], 'example5');
 })();
