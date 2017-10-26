4d7198e2a84284a0a85ef9cba06614145b7a59c3
Michelle Bu <michelle@michellebu.com>
2017-10-25 21:03:06 -0700

    Use options correctly

--- js/example4.js
+++ js/example4.js
@@ -1,71 +1,75 @@
 (function() {
-  'use strict';
+  "use strict";
 
   var elements = stripe.elements({
-    cssSrc: 'https://rsms.me/inter/inter-ui.css',
+    fonts: [
+      {
+        cssSrc: "https://rsms.me/inter/inter-ui.css"
+      }
+    ]
   });
 
   /**
    * Card Element
    */
-  var card = elements.create('card', {
+  var card = elements.create("card", {
     style: {
       base: {
-        color: '#32325D',
+        color: "#32325D",
         fontWeight: 500,
-        fontFamily: 'Inter UI, Open Sans, Segoe UI, sans-serif',
-        fontSize: '15px',
-        fontSmoothing: 'antialiased',
+        fontFamily: "Inter UI, Open Sans, Segoe UI, sans-serif",
+        fontSize: "15px",
+        fontSmoothing: "antialiased",
 
-        '::placeholder': {
-          color: '#CFD7DF',
-        },
+        "::placeholder": {
+          color: "#CFD7DF"
+        }
       },
       invalid: {
-        color: '#E25950',
-      },
-    },
+        color: "#E25950"
+      }
+    }
   });
 
-  card.mount('#example4-card');
+  card.mount("#example4-card");
 
   /**
    * Payment Request Element
    */
   var paymentRequest = stripe.paymentRequest({
-    country: 'US',
-    currency: 'usd',
+    country: "US",
+    currency: "usd",
     total: {
       amount: 2000,
-      label: 'Total',
-    },
+      label: "Total"
+    }
   });
-  paymentRequest.on('token', function(result) {
-    var example = document.querySelector('.example4');
-    example.querySelector('.token').innerText = result.token.id;
-    example.classList.add('submitted');
-    result.complete('success');
+  paymentRequest.on("token", function(result) {
+    var example = document.querySelector(".example4");
+    example.querySelector(".token").innerText = result.token.id;
+    example.classList.add("submitted");
+    result.complete("success");
   });
 
-  var paymentRequestElement = elements.create('paymentRequestButton', {
+  var paymentRequestElement = elements.create("paymentRequestButton", {
     paymentRequest: paymentRequest,
     style: {
       paymentRequestButton: {
-        type: 'donate',
-      },
-    },
+        type: "donate"
+      }
+    }
   });
 
   paymentRequest.canMakePayment().then(function(result) {
     if (result) {
-      document.querySelector('.example4 .card-only').style.display = 'none';
+      document.querySelector(".example4 .card-only").style.display = "none";
       document.querySelector(
-        '.example4 .payment-request-available'
+        ".example4 .payment-request-available"
       ).style.display =
-        'block';
-      paymentRequestElement.mount('#example4-paymentRequest');
+        "block";
+      paymentRequestElement.mount("#example4-paymentRequest");
     }
   });
 
-  registerElements([card, paymentRequestElement], 'example4');
+  registerElements([card, paymentRequestElement], "example4");
 })();
--- js/example5.js
+++ js/example5.js
@@ -1,74 +1,74 @@
 (function() {
-  'use strict';
+  "use strict";
 
   var elements = stripe.elements();
 
   /**
    * Card Element
    */
-  var card = elements.create('card', {
+  var card = elements.create("card", {
+    iconStyle: "solid",
     style: {
-      iconStyle: 'solid',
       base: {
-        iconColor: '#fff',
-        color: '#fff',
+        iconColor: "#fff",
+        color: "#fff",
         fontWeight: 400,
-        fontFamily: 'Helvetica Neue, Helvetica, Arial, sans-serif',
-        fontSize: '15px',
-        fontSmoothing: 'antialiased',
+        fontFamily: "Helvetica Neue, Helvetica, Arial, sans-serif",
+        fontSize: "15px",
+        fontSmoothing: "antialiased",
 
-        '::placeholder': {
-          color: '#BFAEF6',
+        "::placeholder": {
+          color: "#BFAEF6"
         },
-        ':-webkit-autofill': {
-          color: '#fce883',
-        },
+        ":-webkit-autofill": {
+          color: "#fce883"
+        }
       },
       invalid: {
-        iconColor: '#FFC7EE',
-        color: '#FFC7EE',
-      },
-    },
+        iconColor: "#FFC7EE",
+        color: "#FFC7EE"
+      }
+    }
   });
-  card.mount('#example5-card');
+  card.mount("#example5-card");
 
   /**
    * Payment Request Element
    */
   var paymentRequest = stripe.paymentRequest({
-    country: 'US',
-    currency: 'usd',
+    country: "US",
+    currency: "usd",
     total: {
       amount: 2500,
-      label: 'Total',
-    },
+      label: "Total"
+    }
   });
-  paymentRequest.on('token', function(result) {
-    var example = document.querySelector('.example5');
-    example.querySelector('.token').innerText = result.token.id;
-    example.classList.add('submitted');
-    result.complete('success');
+  paymentRequest.on("token", function(result) {
+    var example = document.querySelector(".example5");
+    example.querySelector(".token").innerText = result.token.id;
+    example.classList.add("submitted");
+    result.complete("success");
   });
 
-  var paymentRequestElement = elements.create('paymentRequestButton', {
+  var paymentRequestElement = elements.create("paymentRequestButton", {
     paymentRequest: paymentRequest,
     style: {
       paymentRequestButton: {
-        theme: 'light',
-      },
-    },
+        theme: "light"
+      }
+    }
   });
 
   paymentRequest.canMakePayment().then(function(result) {
     if (result) {
-      document.querySelector('.example5 .card-only').style.display = 'none';
+      document.querySelector(".example5 .card-only").style.display = "none";
       document.querySelector(
-        '.example5 .payment-request-available'
+        ".example5 .payment-request-available"
       ).style.display =
-        'block';
-      paymentRequestElement.mount('#example5-paymentRequest');
+        "block";
+      paymentRequestElement.mount("#example5-paymentRequest");
     }
   });
 
-  registerElements([card], 'example5');
+  registerElements([card], "example5");
 })();
